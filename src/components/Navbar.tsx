9a66b9cc71878a01254a6ed95dc995de03b2a6ab
Gopal <gopal.lohar.dev@gmail.com>
2024-09-05 21:46:34 +0530

    create login

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import { useTransition } from "react";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { cn } from "@/lib/utils";
+import SignIn from "./SignIn";
+export default function Navbar() {
+  return (
+    <nav className="h-16 w-full border-b sm:border-b">
+      <div className="mx-auto flex h-full w-full max-w-[1500px] items-center gap-2 px-2">
+        <Link href="/" className="flex h-full items-center gap-4">
+          <span className="hidden translate-y-0.5 text-xl font-bold text-muted-foreground sm:inline-block">
+            VATAVARAN
+          </span>
+        </Link>
+        <div className="ml-auto flex items-center gap-2">
+          {true ? <ProfilePicButton /> : <LoginButton />}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function ProfilePicButton() {
+  return (
+    <div className="ml-auto flex items-center gap-4 pr-4">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="secondary" size="icon" className="rounded-full p-0">
+            <AccountIcon />
+            <span className="sr-only">Toggle user menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>
+            <div className="px-2 py-1.5 text-sm font-normal">
+              <div className="flex flex-col space-y-1">
+                <p className="text-sm font-medium leading-none">User Name</p>
+                <p className="text-xs leading-none text-muted-foreground">
+                  Email or Something else
+                </p>
+              </div>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Dashboard</DropdownMenuItem>
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Github</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={(e) => {
+              e.preventDefault();
+              // signOut();
+            }}
+          >
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
+
+function LoginButton() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          variant="outline"
+          className="gap-2 rounded-full px-2 text-primary hover:text-primary sm:pr-3"
+        >
+          <AccountIcon />
+          <span className="text hidden sm:inline-block">Login</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Login</DialogTitle>
+          <DialogDescription>
+            Login to connect with you friends online
+          </DialogDescription>
+        </DialogHeader>
+        <SignIn />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  color?: string;
+  width?: number;
+  height?: number;
+  className?: string;
+}
+
+export const AccountIcon = ({
+  color = "currentcolor",
+  width = 24,
+  height = 24,
+  className,
+  ...props
+}: IconProps) => (
+  <svg
+    width={width}
+    height={height}
+    className={cn("shrink-0", className)}
+    fill={color}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 -960 960 960"
+    {...props}
+  >
+    <path d="M240.92-268.31q51-37.84 111.12-59.77Q412.15-350 480-350t127.96 21.92q60.12 21.93 111.12 59.77 37.3-41 59.11-94.92Q800-417.15 800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 62.85 21.81 116.77 21.81 53.92 59.11 94.92ZM480.01-450q-54.78 0-92.39-37.6Q350-525.21 350-579.99t37.6-92.39Q425.21-710 479.99-710t92.39 37.6Q610-634.79 610-580.01t-37.6 92.39Q534.79-450 480.01-450ZM480-100q-79.15 0-148.5-29.77t-120.65-81.08q-51.31-51.3-81.08-120.65Q100-400.85 100-480t29.77-148.5q29.77-69.35 81.08-120.65 51.3-51.31 120.65-81.08Q400.85-860 480-860t148.5 29.77q69.35 29.77 120.65 81.08 51.31 51.3 81.08 120.65Q860-559.15 860-480t-29.77 148.5q-29.77 69.35-81.08 120.65-51.3 51.31-120.65 81.08Q559.15-100 480-100Zm0-60q54.15 0 104.42-17.42 50.27-17.43 89.27-48.73-39-30.16-88.11-47Q536.46-290 480-290t-105.77 16.65q-49.31 16.66-87.92 47.2 39 31.3 89.27 48.73Q425.85-160 480-160Zm0-350q29.85 0 49.92-20.08Q550-550.15 550-580t-20.08-49.92Q509.85-650 480-650t-49.92 20.08Q410-609.85 410-580t20.08 49.92Q450.15-510 480-510Zm0-70Zm0 355Z" />
+  </svg>
+);
---
+++ src/components/SignIn.tsx
@@ -0,0 +1,208 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { useEffect, useState, useTransition } from "react";
+import { z } from "zod";
+import { redirect, useSearchParams } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const loginSchema = z.object({
+  email: z.string().optional(),
+  password: z.string().min(1, "Password is required"),
+});
+
+type LoginCredentials = z.infer<typeof loginSchema>;
+type Errors = Partial<Record<keyof LoginCredentials, string[]>>;
+
+export default function SignIn() {
+  const searchParams = useSearchParams();
+  const redirectUrl = searchParams.get("redirect");
+
+  const [loginCredentials, setLoginCredentials] = useState<LoginCredentials>({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState<Errors>({});
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSubmitting, startTransitioning] = useTransition();
+
+  const setLoginCredential = (
+    property: keyof LoginCredentials,
+    value: string
+  ) => {
+    setLoginCredentials((prev) => ({
+      ...prev,
+      [property]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: any) => {
+    const result = loginSchema.safeParse(loginCredentials);
+    if (result.success) {
+      setErrors(() => ({}));
+      startTransitioning(async () => {
+        let res;
+        try {
+          // res = await signIn("credentials", {
+          //   redirect: false,
+          //   username: result.data.email,
+          //   password: result.data.password,
+          // });
+        } catch (error) {}
+        if (res === "CredentialsSignin") {
+          // if (res?.error === "CredentialsSignin") {
+        } else {
+          if (redirectUrl) {
+            redirect(redirectUrl);
+          }
+          redirect("/");
+        }
+      });
+    } else {
+      const formattedErrors: Errors = {};
+      const errorObject = result.error.format();
+
+      (Object.keys(errorObject) as Array<keyof LoginCredentials>).forEach(
+        (key) => {
+          const error = errorObject[key];
+          if (error && error._errors?.length > 0) {
+            formattedErrors[key] = error._errors;
+          }
+        }
+      );
+      setErrors(formattedErrors);
+    }
+  };
+
+  if (false) {
+    // if (session) {
+    return (
+      <div className="mt-8 text-center">
+        You are logged In as{" "}
+        {/* <span className="font-bold">{session.user.name}</span> */}
+      </div>
+    );
+  } else {
+    return (
+      <form action={handleSubmit}>
+        <div className="mt-4">
+          <Label>Email</Label>
+          <Input
+            name="email"
+            value={loginCredentials.email}
+            placeholder="Enter your email here"
+            onChange={(e) => {
+              setLoginCredential("email", e.target.value);
+            }}
+          />
+          {errors.email && (
+            <p className="text-sm text-destructive">{errors.email[0]}</p>
+          )}
+        </div>
+        <div className="mt-4">
+          <Label>Password</Label>
+          <div className="relative">
+            <Input
+              name="password"
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter your password here"
+              value={loginCredentials.password}
+              onChange={(e) => {
+                setLoginCredential("password", e.target.value);
+              }}
+            />
+            <Button
+              variant="ghost"
+              size="icon"
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                setShowPassword(!showPassword);
+              }}
+              className="bg-background absolute right-0.5 top-0.5 size-8 rounded-full  hover:bg-background"
+            >
+              {showPassword ? <ShowPassWordIcon /> : <HidePassWordIcon />}
+            </Button>
+          </div>
+          {errors.password && (
+            <p className="text-sm text-destructive">{errors.password[0]}</p>
+          )}
+          <div className="mt-2 flex justify-end">
+            <Link
+              className="text-sm text-blue-500 hover:underline"
+              href="/forgot-password"
+            >
+              Forgot Password?
+            </Link>
+          </div>
+        </div>
+        <Button className="mt-8 w-full" type="submit" disabled={isSubmitting}>
+          {isSubmitting ? (
+            <div className="size-6 animate-spin rounded-full border-2 border-primary border-t-primary-foreground"></div>
+          ) : (
+            "Login"
+          )}
+        </Button>
+        <div className="mt-4 text-center">
+          Don&apos;t have an Account?{" "}
+          <Link href="#" className="text-sm text-blue-500 hover:underline">
+            Contact Us
+          </Link>
+        </div>
+      </form>
+    );
+  }
+}
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  color?: string;
+  width?: number;
+  height?: number;
+  className?: string;
+}
+
+export const ShowPassWordIcon = ({
+  color = "currentcolor",
+  width = 24,
+  height = 24,
+  className,
+  ...props
+}: IconProps) => (
+  <svg
+    width={width}
+    height={height}
+    className={cn("shrink-0", className)}
+    fill={color}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 -960 960 960"
+    {...props}
+  >
+    {" "}
+    <path d="M595.08-615.08q24.38 24.39 37.69 59.27 13.31 34.89 10.07 70.04 0 11.54-8.3 19.54-8.31 8-19.85 8-11.54 0-19.54-8t-8-19.54q3.85-26.38-4.34-50-8.19-23.61-25.19-40.61t-41-25.81q-24-8.81-50.62-4.58-11.54.39-19.92-7.73-8.39-8.11-8.77-19.65-.39-11.54 7.42-19.93 7.81-8.38 19.35-8.76 34.92-4 70.19 9.11 35.27 13.12 60.81 38.65ZM480-720q-21.31 0-41.81 2.08-20.5 2.07-40.81 6.84-12.77 2.62-23-3.65t-14.07-18.04q-3.85-12.15 2.54-23.11 6.38-10.96 18.53-13.58 24.16-5.77 48.81-8.15Q454.85-780 480-780q128.92 0 236.85 67 107.92 67 165.99 181.31 4 7.61 5.81 15.34 1.81 7.73 1.81 16.35 0 8.62-1.5 16.35-1.5 7.73-5.5 15.34-18.38 38.46-44.69 71.73t-57.93 61.12q-9.3 8.31-21.26 6.88-11.97-1.42-19.66-11.96-7.69-10.54-6.38-22.61 1.31-12.08 10.61-20.39 27.08-24.54 49.39-53.65Q815.85-466.31 832-500q-50-101-144.5-160.5T480-720Zm0 500q-126.31 0-231.54-67.5Q143.23-355 81.16-465.31q-5-7.61-7.31-16.54-2.31-8.92-2.31-18.15 0-9.23 2-17.85 2-8.61 7-16.84 22.31-40.77 50.54-77.66 28.23-36.88 64.92-66.11l-90.31-90.93q-8.3-8.92-8.19-21.19.12-12.27 8.81-20.96 8.69-8.69 21.08-8.69 12.38 0 21.07 8.69l663.08 663.08q8.31 8.31 8.81 20.57.5 12.27-8.81 21.58-8.69 8.69-21.08 8.69-12.38 0-21.07-8.69L628.62-245.85q-35.39 13.69-72.74 19.77Q518.54-220 480-220ZM238.16-636.31q-35.16 27.16-63.2 61.42Q146.92-540.62 128-500q50 101 144.5 160.5T480-280q25.77 0 50.73-3.46 24.96-3.46 49.58-10.69L529.69-346q-12.15 5.31-24.27 7.19-12.11 1.89-25.42 1.89-68.08 0-115.58-47.5T316.92-500q0-13.31 2.08-25.42 2.08-12.12 7-24.27l-87.84-86.62ZM541-531Zm-131.77 65.77Z" />
+  </svg>
+);
+
+export const HidePassWordIcon = ({
+  color = "currentcolor",
+  width = 24,
+  height = 24,
+  className,
+  ...props
+}: IconProps) => (
+  <svg
+    width={width}
+    height={height}
+    className={cn("shrink-0", className)}
+    fill={color}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 -960 960 960"
+    {...props}
+  >
+    {" "}
+    <path d="M480.09-336.92q67.99 0 115.49-47.59t47.5-115.58q0-67.99-47.59-115.49t-115.58-47.5q-67.99 0-115.49 47.59t-47.5 115.58q0 67.99 47.59 115.49t115.58 47.5ZM480-392q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 172q-126.31 0-231.04-67.39-104.73-67.38-167.19-177.3-5-8.62-7.31-17.37-2.3-8.75-2.3-17.96t2.3-17.94q2.31-8.73 7.31-17.35 62.46-109.92 167.19-177.3Q353.69-780 480-780q126.31 0 231.04 67.39 104.73 67.38 167.19 177.3 5 8.62 7.31 17.37 2.3 8.75 2.3 17.96t-2.3 17.94q-2.31 8.73-7.31 17.35-62.46 109.92-167.19 177.3Q606.31-220 480-220Zm0-280Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z" />
+  </svg>
+);
