81fbc5471ed4df7f115ffa983794b0c4cf4069d3
Gopal <gopal.lohar.dev@gmail.com>
2024-09-05 20:58:30 +0530

    remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import Image from "next/image";
-import Navbar from "./components/Navbar";
-import MiningForm from "./components/Miningform";
 export default function Home() {
-  return (
-    <>
-    <Navbar/>
-    <div className="mt-[5%]">
-      <MiningForm />
-    </div>
-    
-    </>
-  );
+  return <div>Hii</div>;
 }
