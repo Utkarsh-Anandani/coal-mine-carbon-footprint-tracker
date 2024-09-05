d3ac2add87b8e0827ac4c604ad11769cad6837d4
Rudransh Shrivastava <rudransh.shr@gmail.com>
2024-09-06 03:22:26 +0530

    fix use server in action

--- src/actions/saveMineData.ts
+++ src/actions/saveMineData.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { connectDB } from "@/lib/mongodb";
 import MineData from "@/models/MineData";
 import { getServerSession } from "next-auth";
@@ -7,7 +9,16 @@ import { zfd } from "zod-form-data";
 
 const FugitiveEmissionsSchema = zfd.formData({
   coalMined: zfd.numeric(),
-  degree: zfd.text(z.enum([ "seam-degree-I", "seam-degree-II", "seam-degree-III", "mine-degree-I", "mine-degree-II", "mine-degree-III" ])),
+  degree: zfd.text(
+    z.enum([
+      "seam-degree-I",
+      "seam-degree-II",
+      "seam-degree-III",
+      "mine-degree-I",
+      "mine-degree-II",
+      "mine-degree-III",
+    ])
+  ),
 });
 
 const FuelUsageSchema = zfd.formData({
@@ -36,20 +47,20 @@ const MineSchema = zfd.formData({
 const MineDataSchema = zfd.formData({
   mines: z.array(MineSchema),
   from: zfd.text(z.date()),
-  to: zfd.text(z.date()), 
-})
+  to: zfd.text(z.date()),
+});
 
 export async function saveMineData(formData: FormData) {
   await connectDB();
   const safeData = MineDataSchema.safeParse(formData);
-  if(safeData.success === false) {
+  if (safeData.success === false) {
     throw new Error("Invalid data");
   }
   const data = safeData.data;
   // get session
   const { data: session } = useSession();
   const user = session?.user?.email;
-  if(!user) {
+  if (!user) {
     throw new Error("User not found");
   }
   // save data
@@ -58,7 +69,7 @@ export async function saveMineData(formData: FormData) {
     mines: data.mines,
     from: new Date(data.from),
     to: new Date(data.to),
-  })
+  });
 
   console.log("saved data", savedData);
 }
